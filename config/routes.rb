6b7d5de4c1fff02affc5c9d71500174784b3c8b7
iraholmes <ira.holmes63@gmail.com>
2023-03-29 16:14:57 -0700

    Story 2 completed show file added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/blogs' => 'blog#index'
+
+  get '/blogs/:id' => 'blog#show', as: 'blog'
+
+  
 end
